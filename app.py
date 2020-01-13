623a753ed66ff4f12a6e8e1cc5ea95766731f0ec
otsukan <54186620+otsukan@users.noreply.github.com>
2020-01-13 14:59:45 -0700

    Connected flask to d3.json and saved the first three factors into their own dictionaries ready for graphing

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, jsonify
 from flask_pymongo import PyMongo 
+import json
+from bson import json_util
+from bson.json_util import dumps
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/economic_data_db'
 mongo = PyMongo(app)
@@ -9,8 +14,17 @@ mongo = PyMongo(app)
 @app.route('/')
 
 def index():
+    return render_template('index.html')
+    
+@app.route('/data')
+
+def jsondata():
     data = mongo.db.worldbank_data.find()
-    return render_template('index.html', data=data)
+    json_data = []
+    for d in data:
+        json_data.append(d)
+    json_data = json.dumps(json_data, default=json_util.default)
+    return json_data
 
 
 if __name__ =='__main__':
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,25 @@
-d3.json('https://api.db.nomics.world/v22/series/OECD/FDI_AGGR_SUMM/A.AUS.SUB.DI.LE_FA_F?observations=1', function(data) {
+d3.json('http://127.0.0.1:5000/data').then(function(data) {
     console.log(data)
-})
+    
+    var gdp = [];
+    var unemployment = [];
+    var government_spending = [];
+
+    for (var j = 0; j < data.length; j++) {
+        if(data[j]['data_type'] === 'GDP growth (annual %)') {
+            gdp.push(data[j]);
+        }
+        if(data[j]['data_type'] === 'General government final consumption expenditure (% of GDP)') {
+            government_spending.push(data[j])
+        }
+        if(data[j]['data_type'] === 'Unemployment, total (% of total labor force) (modeled ILO estimate)') {
+            unemployment.push(data[j])
+        }
+    }
+    console.log('Gdp:', gdp)
+    console.log('Government_spending', government_spending)
+    console.log('Unemployment', unemployment)
+}).catch(function(error) {
+    console.log(error);
+});
+  
